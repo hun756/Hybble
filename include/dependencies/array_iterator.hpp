19d112b3dfdbe28ec08819f8b5a9800e5578312f
hun756 <ekemenms@hotmail.com>
2020-10-23 20:18:28 +0300

    Added dependencies
    Dependencies added. These dependencies include core, iteration and node classes for
     the respective containers.

---
+++ include/dependencies/array_iterator.hpp
@@ -0,0 +1,14 @@
+/**
+ *  File        :   array_iterator.hpp
+ *  Explain     :   Iterator class for Array.
+ *                  This class contains 3 iterator class.
+ *                      -   iterator
+ *                      -   const_iterator
+ *                      -   reverse_iterator   
+ *  Author      :   Mehmet Ekemen
+ *  Nickname    :   `hun
+ *  Email       :   ekemenms@hotmail.com
+ *  Date        :   22.10.2020
+ *  Github      :   github.com/hun756
+ *  Resource    :   https://github.com/hun756/Data-Sutructures-in-CPlusPlus/
+**/
