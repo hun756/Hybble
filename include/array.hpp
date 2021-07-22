bb100fbe5bd154a256b36a23fb515e38e93ede53
hun756 <ekemenms@hotmail.com>
2021-07-22 19:41:11 +0300

    Update Array
        -  Some Refactoring

--- include/array.hpp
+++ include/array.hpp
@@ -20,272 +20,272 @@
 #include "dependencies/array_iterator.hpp"  //> iterator classes for iterative operations of Array class.
 #include <array>
 
+namespace DSinCpp {
+    template <class T, size_t MaxSize>
+    class Array
+    {
+    public:
+        using const_iterator            =   ConstIterator<T, MaxSize>;
+        using iterator                  =   Iterator<T, MaxSize>;
+        using reverse_iterator          =   ReverseIterator<T, MaxSize>;
+        using const_reverse_iterator    =   ConstReverseIterator<T, MaxSize>;
 
-template <class T, size_t MaxSize>
-class Array
-{
-public:
-    using const_iterator            =   ConstIterator<T, MaxSize>;
-    using iterator                  =   Iterator<T, MaxSize>;
-    using reverse_iterator          =   ReverseIterator<T, MaxSize>;
-    using const_reverse_iterator    =   ConstReverseIterator<T, MaxSize>;
+    public:
 
-public:
-
-    /**
-    *  @brief
-    *      Construct a new DArray object
-   **/
-    Array() = default;
-
-
-    /**
-     *  @brief
-     *      Construct a new DArray object
-     *
-     *  @param  l
-     *      Object from initializer_list class.
+        /**
+        *  @brief
+        *      Construct a new DArray object
     **/
-    Array(std::initializer_list<T>);
+        Array() = default;
 
 
-    friend bool operator ==(const Array<T, MaxSize>& lhs, const Array<T, MaxSize>& rhs);
+        /**
+        *  @brief
+        *      Construct a new DArray object
+        *
+        *  @param  l
+        *      Object from initializer_list class.
+        **/
+        Array(std::initializer_list<T>);
 
 
-    /**
-     *  @brief
-     *      This method checks if the array is empty.
-     *
-     *  @return
-     *      value of bool
-     *
-     *  @retval
-     *      true if index == 0
-    **/
-    [[nodiscard]] const bool empty() const;
+        friend bool operator ==(const Array<T, MaxSize>& lhs, const Array<T, MaxSize>& rhs);
 
 
-    /**
-     *  @brief
-     *      This method returns the number of elements of the array.
-     *
-     *  @return
-     *      size_t
-     *
-     *  @retval
-     *      index
-    **/
-    [[nodiscard]] const size_t size() const;
+        /**
+        *  @brief
+        *      This method checks if the array is empty.
+        *
+        *  @return
+        *      value of bool
+        *
+        *  @retval
+        *      true if index == 0
+        **/
+        [[nodiscard]] const bool empty() const;
 
 
-    /**
-     *  @brief
-     *      This method returns the maximum number of elements
-     *      the array can hold.
-     *
-     *  @return
-     *      <size_t> maxsize value.
-    **/
-    [[nodiscard]] const size_t maxSize() const;
+        /**
+        *  @brief
+        *      This method returns the number of elements of the array.
+        *
+        *  @return
+        *      size_t
+        *
+        *  @retval
+        *      index
+        **/
+        [[nodiscard]] const size_t size() const;
 
 
-    /**
-     *  @brief
-     *      This method takes the desired array element as a parameter.
-     *
-     *  @param[in]
-     *      It is the index of the directory to be accessed.
-     *
-     *  @return
-     *      T&  reference from template
-    **/
-    T& at(size_t);
+        /**
+        *  @brief
+        *      This method returns the maximum number of elements
+        *      the array can hold.
+        *
+        *  @return
+        *      <size_t> maxsize value.
+        **/
+        [[nodiscard]] const size_t maxSize() const;
 
 
-    /**
-     *  @brief
-     *      This method :  returns first element in the array.
-     *
-     *  @return
-     *      the first element of the array.
-    **/
-    T front() const;
+        /**
+        *  @brief
+        *      This method takes the desired array element as a parameter.
+        *
+        *  @param[in]
+        *      It is the index of the directory to be accessed.
+        *
+        *  @return
+        *      T&  reference from template
+        **/
+        T& at(size_t);
 
 
-    /**
-     *  @brief
-     *      This method :  returns first element in the array.
-     *
-     *  @return
-     *      the last element of the array.
-    **/
-    T back() const;
+        /**
+        *  @brief
+        *      This method :  returns first element in the array.
+        *
+        *  @return
+        *      the first element of the array.
+        **/
+        T front() const;
 
 
-    /**
-     *  @brief
-     *      overloading operator [] : takes the desired array element
-     *      as a parameter.
-     *
-     *  @param[in]
-     *      It is the index of the directory to be accessed.
-     *
-     *  @return
-     *      T&  reference from template
-    **/
-    T& operator [](size_t);
-
-    const T& operator [](size_t) const;
-
-    
-    /**
-     *  swap() method : accessing to array element.
-     * 
-     *  @param      Array&     arr    -> the index of the array to be accessed.
-    **/
-    void swap(Array<T, MaxSize>&);
+        /**
+        *  @brief
+        *      This method :  returns first element in the array.
+        *
+        *  @return
+        *      the last element of the array.
+        **/
+        T back() const;
 
 
-    /* Iterative Methods */
+        /**
+        *  @brief
+        *      overloading operator [] : takes the desired array element
+        *      as a parameter.
+        *
+        *  @param[in]
+        *      It is the index of the directory to be accessed.
+        *
+        *  @return
+        *      T&  reference from template
+        **/
+        T& operator [](size_t);
+
+        const T& operator [](size_t) const;
 
 
-    /**
-     *  @brief
-     *      This method  a pointer that returns the starting point of the array.
-     * 
-     *  @return
-     *      object from class Iterator
-    **/
-    iterator begin();
+        /**
+        *  swap() method : accessing to array element.
+        *
+        *  @param      Array&     arr    -> the index of the array to be accessed.
+        **/
+        void swap(Array<T, MaxSize>&);
 
 
-    /**
-     *  @brief
-     *      This method a pointer that returns the ending point of the array.
-     *
-     *  @return
-     *      object from class Iterator
-    **/
-    iterator end();
+        /* Iterative Methods */
 
 
-    /**
-     *  @brief
-     *      This method a const pointer that returns the ending
-     *      point of the array.
-     *
-     *  @return
-     *      object from class Iterator
-    **/
-    const_iterator begin() const;
+        /**
+        *  @brief
+        *      This method  a pointer that returns the starting point of the array.
+        *
+        *  @return
+        *      object from class Iterator
+        **/
+        iterator begin();
 
 
-    /**
-     *  @brief
-     *      This method a const pointer that returns the ending
-     *      point of the array.
-     *
-     *  @return
-     *      object from class Iterator
-    **/
-    const_iterator end() const;
+        /**
+        *  @brief
+        *      This method a pointer that returns the ending point of the array.
+        *
+        *  @return
+        *      object from class Iterator
+        **/
+        iterator end();
 
 
-    /**
-     *  @brief
-     *      This method a const pointer that returns the starting point
-     *      of the array.
-     *
-     *  @return
-     *      object from class Iterator
-    **/
-    reverse_iterator rbegin();
+        /**
+        *  @brief
+        *      This method a const pointer that returns the ending
+        *      point of the array.
+        *
+        *  @return
+        *      object from class Iterator
+        **/
+        const_iterator begin() const;
 
 
-    /**
-     *  @brief
-     *      This method a const pointer that returns the ending point
-     *      of the array.
-     *
-     *  @return
-     *      object from class Iterator
-    **/
-    reverse_iterator rend();
+        /**
+        *  @brief
+        *      This method a const pointer that returns the ending
+        *      point of the array.
+        *
+        *  @return
+        *      object from class Iterator
+        **/
+        const_iterator end() const;
 
 
-    /**
-     *  @brief
-     *      This method : a const pointer that returns the starting point
-     *      of the array.
-     *
-     *  @return
-     *      object from class Iterator
-    **/
-    const_reverse_iterator rbegin() const;
+        /**
+        *  @brief
+        *      This method a const pointer that returns the starting point
+        *      of the array.
+        *
+        *  @return
+        *      object from class Iterator
+        **/
+        reverse_iterator rbegin();
 
 
-    /**
-     *  @brief
-     *      This method : a const pointer that returns the ending point
-     *      of the array.
-     *
-     *  @return
-     *      object from class Iterator
-    **/
-    const_reverse_iterator rend() const;
+        /**
+        *  @brief
+        *      This method a const pointer that returns the ending point
+        *      of the array.
+        *
+        *  @return
+        *      object from class Iterator
+        **/
+        reverse_iterator rend();
 
 
-    /**
-     *  @brief
-     *      This method : a const (always) pointer that returns
-     *      the starting point of the array.
-     * 
-     * 
-     *  @return
-     *      object from class ConstIterator
-    **/
-    const_iterator cbegin() const;
+        /**
+        *  @brief
+        *      This method : a const pointer that returns the starting point
+        *      of the array.
+        *
+        *  @return
+        *      object from class Iterator
+        **/
+        const_reverse_iterator rbegin() const;
 
 
-    /**
-     *  @brief
-     *      This method : a const (always) pointer that returns
-     *      the ending point of the array.
-     * 
-     *  @return
-     *      object from class ConstIterator
-    **/
-    const_iterator cend() const;
+        /**
+        *  @brief
+        *      This method : a const pointer that returns the ending point
+        *      of the array.
+        *
+        *  @return
+        *      object from class Iterator
+        **/
+        const_reverse_iterator rend() const;
 
 
-    /**
-     *  @brief
-     *      This method a pointer that returns the ending point of the array.
-     *      the ending point of the array.
-     *
-     *  @return
-     *      object from class ConstIterator
-    **/
-    const_reverse_iterator crbegin() const;
+        /**
+        *  @brief
+        *      This method : a const (always) pointer that returns
+        *      the starting point of the array.
+        *
+        *
+        *  @return
+        *      object from class ConstIterator
+        **/
+        const_iterator cbegin() const;
 
 
-    /**
-     *  @brief
-     *      This method a pointer that returns the starting
-     *      point of the array.
-     *      The iteration direction is from the end of the array
-     *      to the beginning.
-     *
-     *  @return
-     *      object from class ConstIterator
-    **/
-    const_reverse_iterator crbend() const;
+        /**
+        *  @brief
+        *      This method : a const (always) pointer that returns
+        *      the ending point of the array.
+        *
+        *  @return
+        *      object from class ConstIterator
+        **/
+        const_iterator cend() const;
 
 
-private:
-    T data[MaxSize];
-};
+        /**
+        *  @brief
+        *      This method a pointer that returns the ending point of the array.
+        *      the ending point of the array.
+        *
+        *  @return
+        *      object from class ConstIterator
+        **/
+        const_reverse_iterator crbegin() const;
 
+
+        /**
+        *  @brief
+        *      This method a pointer that returns the starting
+        *      point of the array.
+        *      The iteration direction is from the end of the array
+        *      to the beginning.
+        *
+        *  @return
+        *      object from class ConstIterator
+        **/
+        const_reverse_iterator crbend() const;
+
+
+    private:
+        T data[MaxSize];
+    };
+}
 
 #include "array.tcc"
 
--- src/array.tcc
+++ src/array.tcc
@@ -13,133 +13,139 @@
 #include "array.hpp"
 #include <stdexcept>
 
+namespace DSinCpp {
 
-template<class T, size_t MaxSize>
-Array<T, MaxSize>::Array(std::initializer_list<T> l) 
-{
-    if (l.size() > MaxSize)
-        throw std::out_of_range("Error..! Array limit exceeded.");
-    
-    int i = 0;
-    for (auto iter = l.begin(); iter != l.end(); ++iter)
-        data[i++] = *iter;   
-}
+    template<class T, size_t MaxSize>
+    Array<T, MaxSize>::Array(std::initializer_list<T> l)
+    {
+        if (l.size() > MaxSize)
+            throw std::out_of_range("Error..! Array limit exceeded.");
 
-
-template<class T, size_t MaxSize>
-const bool Array<T, MaxSize>::empty() const
-{
-    return MaxSize == 0;
-}
+        int i = 0;
+        for (auto iter = l.begin(); iter != l.end(); ++iter)
+            data[i++] = *iter;
+    }
 
 
-template<class T, size_t MaxSize>
-const size_t Array<T, MaxSize>::size() const
-{
-    return MaxSize;
-}
+    template<class T, size_t MaxSize>
+    const bool Array<T, MaxSize>::empty() const
+    {
+        return MaxSize == 0;
+    }
 
 
-template<class T, size_t MaxSize>
-const size_t Array<T, MaxSize>::maxSize() const
-{
-    return MaxSize;
-}
+    template<class T, size_t MaxSize>
+    const size_t Array<T, MaxSize>::size() const
+    {
+        return MaxSize;
+    }
 
 
-template<class T, size_t MaxSize>
-inline T& Array<T, MaxSize>::at(size_t index) 
-{
-    if (index >= MaxSize)
-        std::out_of_range("Error..! The index you want to reach is exceed the bounds of the array.");
-    
-    return data[index];
-}
+    template<class T, size_t MaxSize>
+    const size_t Array<T, MaxSize>::maxSize() const
+    {
+        return MaxSize;
+    }
 
 
-template<class T, size_t MaxSize>
-T Array<T, MaxSize>::front() const
-{
-    return data[0];
-}
+    template<class T, size_t MaxSize>
+    inline T& Array<T, MaxSize>::at(size_t index)
+    {
+        if (index >= MaxSize)
+            std::out_of_range("Error..! The index you want to reach is exceed the bounds of the array.");
+
+        return data[index];
+    }
 
 
-template<class T, size_t MaxSize>
-T Array<T, MaxSize>::back() const
-{
-    return data[MaxSize - 1];
-}
+    template<class T, size_t MaxSize>
+    T Array<T, MaxSize>::front() const
+    {
+        return data[0];
+    }
 
 
-template<class T, size_t MaxSize>
-T& Array<T, MaxSize>::operator [](size_t index) 
-{
-    return at(index);
-}
+    template<class T, size_t MaxSize>
+    T Array<T, MaxSize>::back() const
+    {
+        return data[MaxSize - 1];
+    }
 
 
-template<class T, size_t MaxSize>
-const T& Array<T, MaxSize>::operator [](size_t index) const
-{
-    if (index >= MaxSize)
-        std::out_of_range("Error..! The index you want to reach is exceed the bounds of the array.");
-    
-    return data[index];
-}
+    template<class T, size_t MaxSize>
+    T& Array<T, MaxSize>::operator [](size_t index)
+    {
+        return at(index);
+    }
 
 
-template<class T, size_t MaxSize>
-void Array<T, MaxSize>::swap(Array<T, MaxSize>& arr) 
-{
-    Array<T, MaxSize> temp;
-    temp = data;
-    data = arr;
-    arr = temp;
-}
+    template<class T, size_t MaxSize>
+    const T& Array<T, MaxSize>::operator [](size_t index) const
+    {
+        if (index >= MaxSize)
+            std::out_of_range("Error..! The index you want to reach is exceed the bounds of the array.");
+
+        return data[index];
+    }
 
 
-template<class T, size_t MaxSize>
-typename Array<T, MaxSize>::iterator Array<T, MaxSize>::begin() 
-{
-    return iterator(&data[0]);
-}
+    template<class T, size_t MaxSize>
+    void Array<T, MaxSize>::swap(Array<T, MaxSize>& arr) 
+    {
+        Array<T, MaxSize> temp;
+        temp = data;
+        data = arr;
+        arr = temp;
+    }
 
 
-template<class T, size_t MaxSize>
-typename Array<T, MaxSize>::iterator Array<T, MaxSize>::end() 
-{
-    return iterator(&data[MaxSize]);
-}
+    template<class T, size_t MaxSize>
+    typename Array<T, MaxSize>::iterator Array<T, MaxSize>::begin()
+    {
+        return iterator(&data[0]);
+    }
 
 
-template<class T, size_t MaxSize>
-typename Array<T, MaxSize>::const_iterator Array<T, MaxSize>::begin() const
-{
-    // return const_iterator(&data[0]);
-}
+    template<class T, size_t MaxSize>
+    typename Array<T, MaxSize>::iterator Array<T, MaxSize>::end()
+    {
+        return iterator(&data[MaxSize]);
+    }
 
 
-template<class T, size_t MaxSize>
-typename Array<T, MaxSize>::const_iterator Array<T, MaxSize>::end() const
-{
-    // return const_iterator(&data[MaxSize]);
-}
+    template<class T, size_t MaxSize>
+    typename Array<T, MaxSize>::const_iterator Array<T, MaxSize>::begin() const
+    {
+        // return const_iterator(&data[0]);
+    }
 
 
-template<class T, size_t MaxSize>
-typename Array<T, MaxSize>::const_iterator Array<T, MaxSize>::cbegin() const
-{
+    template<class T, size_t MaxSize>
+    typename Array<T, MaxSize>::const_iterator Array<T, MaxSize>::end() const
+    {
+        // return const_iterator(&data[MaxSize]);
+    }
 
-}
+
+    template<class T, size_t MaxSize>
+    typename Array<T, MaxSize>::const_iterator Array<T, MaxSize>::cbegin() const
+    {
+
+    }
 
 
-template<class T, size_t MaxSize>
-typename Array<T, MaxSize>::const_iterator Array<T, MaxSize>::cend() const
-{
+    template<class T, size_t MaxSize>
+    typename Array<T, MaxSize>::const_iterator Array<T, MaxSize>::cend() const
+    {
 
-}
+    }
 
-template<class T, size_t MaxSize>
-bool operator==(const Array<T, MaxSize> &lhs, const Array<T, MaxSize> &rhs) {
-    return false;
+    template<class T, size_t MaxSize>
+    bool operator==(const Array<T, MaxSize>&lhs, const Array<T, MaxSize> &rhs) {
+        for (int i = 0; i < MaxSize; ++i)
+            if (lhs[i] != rhs[i])
+                return false;
+
+        return true;
+    }
 }
