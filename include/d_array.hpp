6c295f57a4a7cd08dc94989622d4897573607eb5
hun756 <ekemenms@hotmail.com>
2021-07-22 19:28:33 +0300

    Update Dynamic Array
        -  Some Code Refactoring.

--- include/d_array.hpp
+++ include/d_array.hpp
@@ -18,160 +18,207 @@
 #include <cstddef>
 
 
-template <class T>
-class DArray
+namespace DSinCpp
 {
-public:
+    template <class T>
+    class DArray
+    {
+    public:
 
-    /**
-     *  @brief
-     *      Construct a new DArray object
-    **/
-    DArray();
-
-
-    /**
-     *  @brief
-     *      Destroy the DArray object
-     */
-    ~DArray();
+        /**
+         *  @brief
+         *      Construct a new DArray object
+        **/
+        DArray();
 
 
-    /**
-     *  @brief
-     *      Construct a new DArray object
-     *
-     *  @param  l
-     *      Object from initializer_list class.
-    **/
-    DArray(std::initializer_list<T>);
+        /**
+         *  @brief
+         *      Destroy the DArray object
+         */
+        ~DArray();
 
 
-    /**
-     *  @brief
-     *      This method checks if the array is empty.
-     *
-     *  @return
-     *      value of bool
-     *
-     *  @retval
-     *      true if index == 0
-    **/
-    [[nodiscard]] const bool empty() const;
+        /**
+         *  @brief
+         *      Construct a new DArray object
+         *
+         *  @param  l
+         *      Object from initializer_list class.
+        **/
+        DArray(std::initializer_list<T>);
 
 
-    /**
-     *  @brief
-     *      This method returns the number of elements of the array.
-     *
-     *  @return
-     *      size_t
-     *
-     *  @retval
-     *      index
-    **/
-    [[nodiscard]] const size_t size() const;
+        /**
+         *  @brief
+         *      Construct a new DArray object
+         *
+         *  @param[in] dArr
+        **/
+        DArray(const DArray<T>& dArr);
 
 
-    /**
-     *  @brief
-     *      This method is the maximum number of elements the array
-     *      can take before dynamic enlargement or reduction.
-     *
-     *  @return
-     *      <size_t> capacity value.
-    **/
-    [[nodiscard]] const size_t capacity() const;
+        /**
+         *  @brief
+         *      Construct a new DArray object
+         *
+         *  @param dArr
+        **/
+        DArray(DArray<T>&& dArr) noexcept;
 
 
-    /**
-     *  @brief
-     *      This method : brings capacity to the value of size.
-    **/
-    void shrinkToFit();
-
-    
-    /**
-     *  @brief
-     *      This method takes the desired array element as a parameter.
-     *
-     *  @param[in]
-     *      It is the index of the directory to be accessed.
-     *
-     *  @return
-     *      T&  reference from template
-    **/
-    T& at(size_t);
-
-    const T& at(size_t) const;
+        /**
+         *  @brief
+         *      Overlod operator ==.
+         *
+         *  @tparam U
+         *  @param[in] lhs
+         *  @param[in] rhs
+         *  @return true
+         *  @retval booelan
+        **/
+        template<class U>
+        friend bool operator == (DArray<U>& lhs, DArray<U>& rhs);
 
 
-    /**
-     *  @brief
-     *      overloading operator [] : takes the desired array element
-     *      as a parameter.
-     *
-     *  @param[in]
-     *      It is the index of the directory to be accessed.
-     *
-     *  @return
-     *      T&  reference from template
-    **/
-    T& operator [](size_t);
-
-    const T& operator [](size_t)  const;
+        /**
+         *  @brief
+         *      This method checks if the array is empty.
+         *
+         *  @return
+         *      value of bool
+         *
+         *  @retval
+         *      true if index == 0
+        **/
+        [[nodiscard]] const bool empty() const;
 
 
-    /**
-     *  @brief
-     *      This method :  returns first element in the array.
-     * 
-     *  @return
-     *      the first element of the array.
-    **/
-    T front() const;
+        /**
+         *  @brief
+         *      This method returns the number of elements of the array.
+         *
+         *  @return
+         *      size_t
+         *
+         *  @retval
+         *      index
+        **/
+        [[nodiscard]] const size_t size() const;
 
 
-    /**
-     *  @brief
-     *      This method :  returns first element in the array.
-     *
-     *  @return
-     *      the last element of the array.
-    **/
-    T back() const;
+        /**
+         *  @brief
+         *      This method is the maximum number of elements the array
+         *      can take before dynamic enlargement or reduction.
+         *
+         *  @return
+         *      <size_t> capacity value.
+        **/
+        [[nodiscard]] const size_t capacity() const;
 
 
-    /**
-     *  @brief
-     *      This method adds an element to the ending of
-     *      the dynamic array.
-     *
-     *  @param  val
-     *      It is the value of the element to be added.
-    **/
-    void pushBack(const T&);
+        /**
+         *  @brief
+         *      This method : brings capacity to the value of size.
+        **/
+        void shrinkToFit();
 
 
-    /**
-     *  @brief
-     *      This method removes the last item from list.
-    **/
-    void popBack();
+        /**
+         *  @brief
+         *      This method takes the desired array element as a parameter.
+         *
+         *  @param[in]
+         *      It is the index of the directory to be accessed.
+         *
+         *  @return
+         *      T&  reference from template
+        **/
+        T& at(size_t);
+
+        const T& at(size_t) const;
 
 
-    // template<class... Args>
-    // T& emplaceBack(Args&&... args);
+        /**
+         *  @brief
+         *      overloading operator [] takes the desired array element
+         *      as a parameter.
+         *
+         *  @param[in]
+         *      It is the index of the directory to be accessed.
+         *
+         *  @return
+         *      T&  reference from template
+        **/
+        T& operator [](size_t);
 
-private:
-    void reAlloc();
+        const T& operator [](size_t)  const;
 
 
-private:
-    T*    data;
-    size_t  index;
-    size_t  _capacity;
-};
+        /**
+         *  @brief
+         *      This method :  returns first element in the array.
+         *
+         *  @return
+         *      the first element of the array.
+        **/
+        T front() const;
+
+
+        /**
+         *  @brief
+         *      This method :  returns first element in the array.
+         *
+         *  @return
+         *      the last element of the array.
+        **/
+        T back() const;
+
+
+        /**
+         *  @brief
+         *      This method adds an element to the ending of
+         *      the dynamic array.
+         *
+         *  @param  val
+         *      It is the value of the element to be added.
+        **/
+        void pushBack(const T&);
+
+
+        /**
+         *  @brief
+         *      This method removes the last item from list.
+        **/
+        void popBack();
+
+
+        /**
+         * @brief 
+         * 
+         * @tparam Args 
+         * @param args 
+         * @return T& 
+        **/
+        template<class... Args>
+        T& emplaceBack(Args&&... args);
+
+    private:
+        /**
+         * @brief 
+         * 
+         */
+        void reAlloc();
+
+
+    private:
+        T*    data;
+        size_t  index;
+        size_t  _capacity;
+    };
+}
+
 
 //> template implementation..
 #include "d_array.tcc"
--- src/d_array.tcc
+++ src/d_array.tcc
@@ -12,163 +12,195 @@
 
 #include <stdexcept>
 
-template<class T>
-DArray<T>::DArray() : data(nullptr), index(), _capacity(4)
+
+namespace DSinCpp
 {
-    data = new T[_capacity];
-}
-
-
-template<class T>
-DArray<T>::~DArray()
-{
-    delete[] data;
-}
-
-
-
-template<class T>
-DArray<T>::DArray(std::initializer_list<T> l) : index(0)
-{
-    _capacity = l.end() - l.begin();
-
-    data = new T[_capacity];
-
-    for (auto listVal : l)
-        data[index++] = listVal;
-}
-
-
-template<class T>
-const bool DArray<T>::empty() const
-{
-    return index == 0;
-}
-
-
-template<class T>
-const size_t DArray<T>::size() const
-{
-    return index;
-}
-
-
-template<class T>
-const size_t DArray<T>::capacity() const
-{
-    return _capacity;
-}
-
-
-template<class T>
-void DArray<T>::shrinkToFit()
-{
-    _capacity = index;
-}
-
-
-template<class T>
-T& DArray<T>::at(size_t i)
-{
-    if (i >= _capacity)
-        std::out_of_range("Error..! The index you want to reach is exceed the bounds of the array.");
-
-    return data[i];
-}
-
-
-template<class T>
-const T& DArray<T>::at(size_t i) const
-{
-    if (i >= _capacity)
-        std::out_of_range("Error..! The index you want to reach is exceed the bounds of the array.");
-
-    return data[i];
-}
-
-
-template<class T>
-T& DArray<T>::operator [](size_t i)
-{
-    return at(i);
-}
-
-
-template<class T>
-const T& DArray<T>::operator [](size_t i) const
-{
-    return at(i);
-}
-
-
-template<class T>
-T DArray<T>::front() const
-{
-    return data + 0;
-}
-
-
-template<class T>
-T DArray<T>::back() const
-{
-    return data + size();
-}
-
-
-template<class T>
-void DArray<T>::pushBack(const T& val)
-{
-    if (size() == capacity())
+    template<class T>
+    DArray<T>::DArray() : data(nullptr), index(), _capacity(4)
     {
-        _capacity *= 2;
-        reAlloc();
+        data = new T[_capacity];
     }
 
-    data[index++] = val;
-}
 
-
-template<class T>
-void DArray<T>::popBack()
-{
-    if (empty())
-        throw std::out_of_range("Error...!");
-
-    data[index--].~T();
-
-    if ((size() == (capacity() / 2)) && (size() >= 4))
+    template<class T>
+    DArray<T>::~DArray()
     {
-        _capacity /= 2;
-        reAlloc();
+        delete[] data;
     }
 
-}
+
+    template<class T>
+    DArray<T>::DArray(std::initializer_list<T> l) : index(0)
+    {
+        _capacity = l.end() - l.begin();
+
+        data = new T[_capacity];
+
+        for (auto listVal : l)
+            data[index++] = listVal;
+    }
 
 
-// template<class T>
-// template<class... Args>
-// T& DArray<T>::emplaceBack(Args&&... args)
-// {
-//     if (size() >= _capacity)
-//     {
-//        _capacity *= 2;
-//        reAlloc();
-//     }
+    template<class T>
+    DArray<T>::DArray(const DArray<T>& dArr)
+    {
 
-//    data[index] = T(std::forward<Args>(args)...);
-//    return data[index++];
-// }
+    }
 
 
-template<class T>
-void DArray<T>::reAlloc()
-{
-    T* temp = new T[_capacity];
+    template<class T>
+    DArray<T>::DArray(DArray<T>&& dArr) noexcept
+    {
 
-    for (size_t i = 0; i < size(); ++i)
-        temp[i] = data[i];
+    }
 
-    delete[] data;
 
-    data = temp;
+    template<class T>
+    bool operator == (DArray<T>& lhs, DArray<T>& rhs)
+    {
+        if (lhs.size() != rhs.size())
+            return false;
+
+        for (int i = 0; i < lhs.size(); ++i)
+            if (lhs[i] != rhs[i])
+                return false;
+
+        return true;
+    }
+
+
+    template<class T>
+    const bool DArray<T>::empty() const
+    {
+        return index == 0;
+    }
+
+
+    template<class T>
+    const size_t DArray<T>::size() const
+    {
+        return index;
+    }
+
+
+    template<class T>
+    const size_t DArray<T>::capacity() const
+    {
+        return _capacity;
+    }
+
+
+    template<class T>
+    void DArray<T>::shrinkToFit()
+    {
+        _capacity = index;
+    }
+
+
+    template<class T>
+    T& DArray<T>::at(size_t i)
+    {
+        if (i >= _capacity) {
+            throw std::out_of_range("Error..! The index you want to reach is exceed the bounds of the array.");
+        }
+
+        return data[i];
+    }
+
+
+    template<class T>
+    const T& DArray<T>::at(size_t i) const
+    {
+        if (i >= _capacity)
+            throw std::out_of_range("Error..! The index you want to reach is exceed the bounds of the array.");
+
+        return data[i];
+    }
+
+
+    template<class T>
+    T& DArray<T>::operator [](size_t i)
+    {
+        return at(i);
+    }
+
+
+    template<class T>
+    const T& DArray<T>::operator [](size_t i) const
+    {
+        return at(i);
+    }
+
+
+    template<class T>
+    T DArray<T>::front() const
+    {
+        return data + 0;
+    }
+
+
+    template<class T>
+    T DArray<T>::back() const
+    {
+        return data + size();
+    }
+
+
+    template<class T>
+    void DArray<T>::pushBack(const T& val)
+    {
+        if (size() == capacity())
+        {
+            _capacity *= 2;
+            reAlloc();
+        }
+
+        data[index++] = val;
+    }
+
+
+    template<class T>
+    void DArray<T>::popBack()
+    {
+        if (empty())
+            throw std::out_of_range("Error...!");
+
+        data[index--].~T();
+
+        if ((size() == (capacity() / 2)) && (size() >= 4))
+        {
+            _capacity /= 2;
+            reAlloc();
+        }
+
+    }
+
+
+     template<class T>
+     template<class... Args>
+     T& DArray<T>::emplaceBack(Args&&... args)
+     {
+         if (size() >= _capacity)
+         {
+            _capacity *= 2;
+            reAlloc();
+         }
+
+        data[index] = T(std::forward<Args>(args)...);
+        return data[index++];
+     }
+
+
+    template<class T>
+    void DArray<T>::reAlloc()
+    {
+        T* temp = new T[_capacity];
+
+        for (size_t i = 0; i < size(); ++i)
+            temp[i] = data[i];
+
+        delete[] data;
+
+        data = temp;
+    }
 }
